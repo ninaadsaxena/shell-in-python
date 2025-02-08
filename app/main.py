7b07ef4eba13072dde4026d9fefde09ef20455c7
ninaadsaxena <ninaadsaxena19@gmail.com>
2025-02-08 18:25:48 +0530

    exit built-in

--- app/main.py
+++ app/main.py
@@ -3,12 +3,17 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
     
     # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
-    main()
+    #command = input()
+        user_input = input()
+    #print(f"{command}: command not found")
+    #main()
+        if user_input == "exit 0":
+            break
+        sys.stdout.write(f"{user_input}: not found\n")
 
 
 if __name__ == "__main__":
